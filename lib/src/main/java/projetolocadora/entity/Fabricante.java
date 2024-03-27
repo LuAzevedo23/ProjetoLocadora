59258717e4a24d3402dfebaaff0eca3a5747c984
Luciene Azevedo <luciene.azevedo@hotmail.com>
2024-03-26 22:33:23 -0300

    segundo commit
    Atualizado no dia de hoje 26/03/24.

---
+++ lib/src/main/java/projetolocadora/entity/Fabricante.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package projetolocadora.entity;
+
+/**
+ *
+ * @author lucie
+ */
+public class Fabricante {
+    
+    private int id;
+    private String fabricante;
+    
+    public int getId(){
+        return id;
+    
+    
+    
+    }
+        
+
+    
+            
+        }
+    
+    
+
